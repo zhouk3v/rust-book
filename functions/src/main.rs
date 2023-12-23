c39aa3af072315980e0fd79186784d59316aaa99
Kevin Zhou <zhoukev@hotmail.com>
2023-12-23 14:41:30 -0500

    3.3. Functions

---
+++ functions/src/main.rs
@@ -0,0 +1,50 @@
+//
+// Functions
+//
+
+/* 
+fn main() {
+    println!("Hello, world!");
+
+    another_function();
+}
+
+fn another_function() {
+    println!("Another function.");
+}
+*/
+
+//
+// Parameters
+//
+/*
+fn main() {
+    another_function(5);
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+*/
+
+//
+// Multiple parameters
+//
+/*
+fn main() {
+    print_labeled_measurement(5, 'h');
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+*/
+
+//
+// Statements and Expressions
+//
+
+fn main() {
+    // Variable assignment with 'let' is a statement
+    let y = 6;
+}
