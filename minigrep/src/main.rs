83f4029017ae71fd0ae00d16d266df0cc097167b
DESKTOP-FPSVATK\zhouk <zhoukev@hotmail.com>
2024-04-20 16:48:48 -0400

    12.6. Writing Error Messages to Standard Error Instead of Standard Output

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -9,12 +9,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        // The eprintln! macro prints to the standard error stream
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
