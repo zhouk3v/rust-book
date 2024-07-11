37c9b57eeee9e5df33d46563e21b0e0b25efd458
DESKTOP-FPSVATK\zhouk <zhoukev@hotmail.com>
2024-07-10 22:12:33 -0400

    20.2. Turning Our Single-Threaded Server into a Multithreaded Server

--- chapter-20/hello/src/main.rs
+++ chapter-20/hello/src/main.rs
@@ -1,20 +1,26 @@
+use hello::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
     // `TcpListener` will listen for TCP connections at the passed-in address string
     // bind() works like the new() function, it will return a new `TcpListener` instance
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     // incoming() returns an iterator that gives us a sequence of streams of type `TcpStream` (a stream represents a connection between a client and server)
     for stream in listener.incoming() {
         // Read from the `TcpStream` to see what the client sent
         let stream = stream.unwrap();
 
-        handle_connections(stream);
+        pool.execute(|| {
+            handle_connections(stream);
+        });
     }
 }
 
@@ -23,10 +29,13 @@ fn handle_connections(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
